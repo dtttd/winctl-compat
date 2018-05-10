99b0d5380f42d7866987a540915fff59430353c2
Simone Lusenti <lusenti.s@gmail.com>
2018-05-10 19:09:43 +0200

    use @digitalattitude scoped packages, rename

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const os = require("os");
 const IS_WINDOWS = os.platform() === "win32";
 
 if (IS_WINDOWS) {
-  module.exports = require("winctl");
+  module.exports = require("@digitalattitude/winctl");
 } else {
   module.exports = require("./winctl-phony");
 }
